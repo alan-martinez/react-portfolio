c081b569d269d457bcd3e3f42ad59077e49be62b
alan-martinez <alan.martinez5691@alumnos.udg.mx>
2022-05-31 14:22:29 -0500

    React portf

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom'
+import './index.scss';
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                klenbooy
+                <br />
+                web developer
+                </h1>
+                <h2>Frontend Developer / JavaScript / Student</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
